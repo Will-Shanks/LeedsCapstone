b5cf72f061c39ca873e7848adb8995797877d409
Will Shanks <92five@gmail.com>
2019-12-16 18:01:14 -0700

    improving title scraping

--- tasks/task1/getTitles.py
+++ tasks/task1/getTitles.py
@@ -7,12 +7,12 @@ def main():
     with open(sys.argv[1]) as fh:
         lastline = ''
         for line in fh:
-            if line.startswith('History'):
-                if lastline.isupper():
-                    print(lastline, end='')
-                else:
-                    print("MISSED COMPANY NAME")
-            lastline = line
+            l = line.strip()
+            if l.startswith('History'):
+                print("'"+lastline+"'", end=', ')
+                lastline="MISSED COMPANY NAME"
+            if l.isupper():
+                lastline = l
 
 if __name__ == '__main__':
     sys.exit(main())
