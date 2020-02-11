cbd7351224a4e599ac60fc6ba0b95ab4c1f56831
Will Shanks <92five@gmail.com>
2020-02-11 11:19:18 -0700

    updated submit script

--- daysToTitles.sh
+++ daysToTitles.sh
@@ -5,19 +5,22 @@
 #SBATCH --partition=shas
 #BATCH --output=log-%j.out 
 
-DIR='/scratch/summit/diga9728/Moodys/Industrials/OCRrun1930/' #050/OCRoutputIndustrial19300007-005670.day'
+DAY_DIR='/scratch/summit/diga9728/Moodys/Industrials/OCRrun1930/' #050/OCRoutputIndustrial19300007-005670.day'
+
+# update this var to where you cloned the repo to
+PROJECT_DIR='/home/'$USER'/projects/LeedsCapstone'
 
 module purge
 module load python/3.6.5
-rm -f /home/wish9643/projects/output.txt
+rm -f $PROJECT_DIR/output.txt
 
 echo date
 
-for i in $(grep ,1 $DIR/mastercolumns1930.csv | cut -d',' -f1 | cut -c -23); do
-  file=$(find $DIR -name "OCRoutput"$i"70.day" -type f)
-  echo $file":" >> /home/wish9643/projects/output.txt
-  python3 /home/wish9643/projects/LeedsCapstone/tasks/task1/makeText.py $file > /scratch/summit/wish9643/tmp.txt
-  python3 /home/wish9643/projects/LeedsCapstone/tasks/task1/getTitles.py /scratch/summit/wish9643/tmp.txt >> /home/wish9643/projects/output.txt
+for i in $(grep ,1 $DAY_DIR/mastercolumns1930.csv | cut -d',' -f1 | cut -c -23); do
+  file=$(find $DAY_DIR -name "OCRoutput"$i"70.day" -type f)
+  echo $file":" >> $PROJECT_DIR/output.txt
+  python3 $PROJECT_DIR/src/dayToDF.py $file > /scratch/summit/$USER/tmp.txt
+  python3 $PROJECT_DIR/src/getTitles.py /scratch/summit/$USER/tmp.txt >> $PROJECT_DIR/output.txt
 
   echo "" >> /home/wish9643/projects/output.txt
 done
