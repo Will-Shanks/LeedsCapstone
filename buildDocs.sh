aaa5e8d44e497261a7fbc4b61fd1dcc7bc7a2e81
Will Shanks <92five@gmail.com>
2020-02-24 21:44:10 -0700

    built docs

--- buildDocs.sh
+++ buildDocs.sh
@@ -1,3 +1,2 @@
-export PYTHONPATH=./src
 sphinx-apidoc -f -o docs/rst/ src/
 sphinx-build -b html -c docs/ docs/rst/ docs/html/
--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []#'sphinxcontrib.napoleon']
+extensions = ['sphinxcontrib.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
