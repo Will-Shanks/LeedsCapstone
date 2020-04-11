e0d34cbd3ea9b9750ecaf7273621b4d0e5e606fd
Will Shanks <92five@gmail.com>
2020-04-11 16:05:19 -0600

    added usage comment, made arg logic more robust

--- daysToTitles.sh
+++ daysToTitles.sh
@@ -5,27 +5,41 @@
 #SBATCH --partition=shas
 #SBATCH --output=log-%j.out 
 
+
+#daysToTitles.sh Usage: ./daysToTitles.sh <year> [<project dir>]
+#  where year is the year of the manual you wish to get the titles from
+#  project dir is the root of this code repo, and is optional if this script is called from said dir
 date
 
-
-# First argument is the year you want to scrape for
-y=$1
-# second argument takes path to directory one level up from where source code is stored(e.g. /home/kevin/projects where code is in /home/kevin/projects/code)
-PROJECT_DIR=$2
-
-if [ $# != 2 ]
+# check if project dir was supplied
+if [ $# -eq 2 ]
 then
-	echo 'Usage: daysToTitles.sh <year> <path to source code directory (one level up)>'
-	exit
+	PROJECT_DIR=$2
+elif [ $# -eq 1 ]
+# if it wasn't then check if it is current dir
+then
+	# if can find getTitles.py then we're good to go with $PWD
+	if [ -f "$PWD/src/getTitles.py" ]
+	then
+		PROJECT_DIR=$PWD
+	else
+		echo "Error couldn't find $PWD/src/getTitles.py, must supply project dir"
+		exit 1
+	fi
+else
+# if no args given then wrongs, print usage
+	echo "Usage: ./daysToTitles.sh <year> [<project dir>]"
+	exit 1
 fi
 
+YEAR=$1
 
 module purge
 module load python/3.6.5
-rm -f $PROJECT_DIR/output.txt
 
+#probably not neccesary but leave just in case
 export PYTHONPATH=$PROJECT_DIR/src:$PYTHONPATH
 
-python3 $PROJECT_DIR/code/getTitles.py $y >> $PROJECT_DIR/company_names_$y.txt
+python3 $PROJECT_DIR/src/getTitles.py $YEAR > $PROJECT_DIR/company_names_$YEAR.txt
 
 date
