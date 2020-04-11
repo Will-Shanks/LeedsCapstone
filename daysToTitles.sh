f1d04ffc470f46295b399be72ae0f5d3681a5b71
kevmonsta <36528551+kevmonsta@users.noreply.github.com>
2020-04-11 09:42:44 -0600

    Updated Submission script
    I made it so that you can pass in the year you want to scrape for as well as the path to the source code via the command line.
    
    This way we dont have to have multiple copies of this script for each year.

--- daysToTitles.sh
+++ daysToTitles.sh
@@ -7,8 +7,18 @@
 
 date
 
-# update this var to where you cloned the repo to
-PROJECT_DIR='/home/'$USER'/projects/LeedsCapstone'
+
+# First argument is the year you want to scrape for
+y=$1
+# second argument takes path to directory one level up from where source code is stored(e.g. /home/kevin/projects where code is in /home/kevin/projects/code)
+PROJECT_DIR=$2
+
+if [ $# != 2 ]
+then
+	echo 'Usage: daysToTitles.sh <year> <path to source code directory (one level up)>'
+	exit
+fi
+
 
 module purge
 module load python/3.6.5
@@ -16,6 +26,6 @@ rm -f $PROJECT_DIR/output.txt
 
 export PYTHONPATH=$PROJECT_DIR/src:$PYTHONPATH
 
-python3 $PROJECT_DIR/src/getTitles.py 1930 >> $PROJECT_DIR/output.txt
+python3 $PROJECT_DIR/code/getTitles.py $y >> $PROJECT_DIR/company_names_$y.txt
 
 date
